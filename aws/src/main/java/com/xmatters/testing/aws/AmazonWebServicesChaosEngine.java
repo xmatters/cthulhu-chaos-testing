17a54b71202b3bf93dc8b944ed613fe9b0fc4b9b
Gabrielle Gasse <ggasse@xmatters.com>
2019-06-07 13:53:51 -0700

    AWS support - Find vm targets by zone and name tag; delete reset and stop operations.

---
+++ aws/src/main/java/com/xmatters/testing/aws/AmazonWebServicesChaosEngine.java
@@ -0,0 +1,64 @@
+package com.xmatters.testing.aws;
+
+import java.util.Map;
+import java.util.function.Consumer;
+
+import software.amazon.awssdk.services.ec2.model.Ec2Exception;
+
+import com.xmatters.testing.cthulhu.api.annotations.EngineName;
+import com.xmatters.testing.cthulhu.api.annotations.OperationName;
+import com.xmatters.testing.cthulhu.api.eventrunner.ChaosEngine;
+import com.xmatters.testing.cthulhu.api.eventrunner.UpdateType;
+import com.xmatters.testing.cthulhu.api.scenario.ChaosEvent;
+
+@EngineName("aws-ec2")
+public class AmazonWebServicesChaosEngine extends ChaosEngine {
+
+    private static final String TARGET_ZONE = "zone";
+    private static final String TARGET_INSTANCE_NAME = "instanceName";
+
+    private AWSConnector connector;
+
+    @Override
+    public void configure() throws Exception {
+        super.configure();
+        this.connector = new AWSConnector();
+    }
+
+    @Override
+    public String[] getTargets(ChaosEvent ev) {
+        Map<String, String> targetParts = ev.getTargetParts(TARGET_ZONE, TARGET_INSTANCE_NAME);
+        try {
+            return connector.findInstances(targetParts.get(TARGET_ZONE), targetParts.get(TARGET_INSTANCE_NAME));
+        } catch (Exception e) {
+            this.sendUpdate(UpdateType.ERROR, "An error occurred while searching for instances.", e.toString());
+        }
+        return null;
+    }
+
+    @OperationName("delete")
+    public void deleteVm(String[] targets) {
+        tryForEachTarget(targets, "Deleting instance:", target -> connector.delete(target));
+    }
+
+    @OperationName("reset")
+    public void resetVm(String[] targets) {
+        tryForEachTarget(targets, "Resetting instance:", target -> connector.reset(target));
+    }
+
+    @OperationName("stop")
+    public void stopVm(String[] targets) {
+        tryForEachTarget(targets, "Stopping instance:", target -> connector.stop(target));
+    }
+
+    private void tryForEachTarget(String[] targets, String message, Consumer<String> action) {
+        for (String target : targets) {
+            this.sendUpdate(UpdateType.INFO, message, target);
+            try {
+                action.accept(target);
+            } catch (Ec2Exception e) {
+                this.sendUpdate(UpdateType.ERROR, e.toString());
+            }
+        }
+    }
+}
