9f2e18611ea9599c2e2a8cd8c826c1426096ace9
Gabrielle Gasse <ggasse@xmatters.com>
2019-06-07 16:34:35 -0700

    Kube cleanup

--- kubernetes/src/main/java/com/xmatters/testing/kubernetes/DebugRunner.java
+++ kubernetes/src/main/java/com/xmatters/testing/kubernetes/DebugRunner.java
@@ -6,7 +6,6 @@ import io.fabric8.kubernetes.api.model.Pod;
 
 import java.util.HashMap;
 import java.util.Map;
-import java.util.stream.Stream;
 
 public class DebugRunner {
 
@@ -17,28 +16,20 @@ public class DebugRunner {
         // put("kube.config", "/path/to/.kube/config");
     }};
 
-    private static final Map<String, ChaosEvent> chaosEvents = new HashMap<String, ChaosEvent>(){{
-        // Demonstrates how to affect a subset of Pods out of a pool of matches.
-        put("destroyTestPod", new ChaosEvent() {{
-            setDescription("Deleting two random chaos-test-dummy");
-            setEngine("kubernetes");
-            setOperation("delete");
-            setTarget("chaos-test/chaos-test-dummy");
-            setQuantity(2);
-        }});
-    }};
-
     public static void main(String[] args) throws Exception {
         KubeChaosEngine runner = new KubeChaosEngine();
         runner.setConfiguration(CONFIG);
         runner.setUpdateCollector(DebugRunner::updateCollector);
         runner.configure();
 
-        Stream.of(args).forEach(eventName -> {
-            ChaosEvent ev = chaosEvents.getOrDefault(eventName, new ChaosEvent());
-            Pod[] targets = runner.getTargets(ev);
-            runner.deletePod(targets);
-        });
+        ChaosEvent ev = new ChaosEvent() {{
+            setDescription("Deleting chaos-test-dummy");
+            setEngine("kubernetes");
+            setOperation("delete");
+            setTarget("chaos-test/chaos-test-dummy");
+        }};
+        Pod[] targets = runner.getTargets(ev);
+        runner.deletePod(targets);
     }
 
     private static void updateCollector(UpdateType type, String... messages) {
